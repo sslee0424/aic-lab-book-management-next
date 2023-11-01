cbb553d09cb82997f0d78fd4c2531b54515641fc
SUSEON <tntjsdlrk04@gmail.com>
2023-11-02 08:27:02 +0900

    카메라 전면 -> 후면 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,8 @@ useEffect(() => {
 
 useEffect(() => {
  navigator.mediaDevices.getUserMedia({
-   video: { facingMode: "user" }, //전면 카메라
-  //  video: { facingMode: { exact: "environment" } }, //후면 카메라
+  //  video: { facingMode: "user" }, //전면 카메라
+   video: { facingMode: { exact: "environment" } }, //후면 카메라
  })
    .then(stream => {
      console.log(stream);
